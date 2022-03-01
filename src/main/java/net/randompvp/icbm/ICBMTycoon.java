f71d55a8f053dd169a307c011c8d57490bf7d10e
RRRamiroGit <73256855+RRRamiroGit@users.noreply.github.com>
2022-03-01 07:06:50 -0500

    Make each page class-based

--- src/main/java/net/randompvp/icbm/ICBMTycoon.java
+++ src/main/java/net/randompvp/icbm/ICBMTycoon.java
@@ -13,39 +13,66 @@ public class ICBMTycoon {
 	
 	int windowHeight = 576;
 	int windowWidth = 1024;
-	JButton startButton = new JButton("Start!");
-	JButton exitButton = new JButton("Exit");
-	JLabel welcome = new JLabel("Welcome to Ed's ICBM Tycoon! Press the Start button to continue");
-	JLabel author = new JLabel("Made by Ramiro");
+	JPanel panel;
+	Page screen;
 	
 	public ICBMTycoon() {
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setLayout(null);
-		int buttonWidth = 140;
-		int buttonHeight = 50;
-		panel.add(startButton);
-		startButton.setFont(new Font("Arial", Font.PLAIN, 34));
-		startButton.setBounds(332, 238, buttonWidth, buttonHeight);
-		panel.add(exitButton);
-		exitButton.setFont(new Font("Arial", Font.PLAIN, 34));
-		exitButton.setBounds(552, 238, buttonWidth, buttonHeight);
 		frame.add(panel, BorderLayout.CENTER);
-		panel.add(welcome);
-		welcome.setFont(new Font("Arial", Font.PLAIN, 28));
-		welcome.setBounds(102, 50, 820, 32);
-		panel.add(author);
-		author.setBounds(462, 85, 100, 14);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
 		frame.setTitle("Ed's ICBM Tycoon");
         frame.pack();
         frame.setVisible(true);
+        changeScreen(new Welcome());
+	}
+	
+	void changeScreen(Page newScreen) {
+		if (screen != null)
+			screen.remove();
+		screen = newScreen;
 	}
 
 	public static void main(String[] args) {
 		new ICBMTycoon();
+	}
+	
+	abstract class Page {
+		abstract public void remove();
+	}
+	
+	class Welcome extends Page {
+		JButton startButton = new JButton("Start!");
+		JButton exitButton = new JButton("Exit");
+		JLabel welcome = new JLabel("Welcome to Ed's ICBM Tycoon! Press the Start button to continue");
+		JLabel author = new JLabel("Made by Ramiro");
+		
+		public Welcome() {
+			int buttonWidth = 140;
+			int buttonHeight = 50;
+			panel.add(startButton);
+			startButton.setFont(new Font("Arial", Font.PLAIN, 34));
+			startButton.setBounds(332, 238, buttonWidth, buttonHeight);
+			panel.add(exitButton);
+			exitButton.setFont(new Font("Arial", Font.PLAIN, 34));
+			exitButton.setBounds(552, 238, buttonWidth, buttonHeight);
+			panel.add(welcome);
+			welcome.setFont(new Font("Arial", Font.PLAIN, 28));
+			welcome.setBounds(102, 50, 820, 32);
+			panel.add(author);
+			author.setBounds(462, 85, 100, 14);
+		}
+
+		@Override
+		public void remove() {
+			panel.remove(startButton);
+			panel.remove(exitButton);
+			panel.remove(welcome);
+			panel.remove(author);
+		}
 	}
 
 }
