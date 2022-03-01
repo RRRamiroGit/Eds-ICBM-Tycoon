b803aa301c94776d01851ae46c15afed352071cf
RRRamiroGit <73256855+RRRamiroGit@users.noreply.github.com>
2022-03-01 07:48:57 -0500

    Add background and change author

--- src/main/java/net/randompvp/icbm/ICBMTycoon.java
+++ src/main/java/net/randompvp/icbm/ICBMTycoon.java
@@ -1,9 +1,18 @@
 package net.randompvp.icbm;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,11 +22,12 @@ public class ICBMTycoon {
 	
 	int windowHeight = 576;
 	int windowWidth = 1024;
+	JFrame frame;
 	JPanel panel;
 	Page screen;
 	
 	public ICBMTycoon() {
-		JFrame frame = new JFrame();
+		frame = new JFrame();
 		panel = new JPanel();
 		panel.setLayout(null);
 		frame.add(panel, BorderLayout.CENTER);
@@ -26,7 +36,6 @@ public class ICBMTycoon {
 		frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
 		frame.setTitle("Ed's ICBM Tycoon");
         frame.pack();
-        frame.setVisible(true);
         changeScreen(new Welcome());
 	}
 	
@@ -34,6 +43,7 @@ public class ICBMTycoon {
 		if (screen != null)
 			screen.remove();
 		screen = newScreen;
+		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
@@ -48,30 +58,47 @@ public class ICBMTycoon {
 		JButton startButton = new JButton("Start!");
 		JButton exitButton = new JButton("Exit");
 		JLabel welcome = new JLabel("Welcome to Ed's ICBM Tycoon! Press the Start button to continue");
-		JLabel author = new JLabel("Made by Ramiro");
+		JLabel author = new JLabel("Developed by Ramiro");
+		JLabel author2 = new JLabel("Concept by Ed");
+		JLabel icbmImage = initializeImage();
 		
 		public Welcome() {
 			int buttonWidth = 140;
 			int buttonHeight = 50;
-			panel.add(startButton);
+			panel.add(icbmImage);
+			icbmImage.setBounds(0, 0, 1024, 576);
+			icbmImage.setOpaque(true);
+			icbmImage.add(startButton);
 			startButton.setFont(new Font("Arial", Font.PLAIN, 34));
 			startButton.setBounds(332, 238, buttonWidth, buttonHeight);
-			panel.add(exitButton);
+			icbmImage.add(exitButton);
 			exitButton.setFont(new Font("Arial", Font.PLAIN, 34));
 			exitButton.setBounds(552, 238, buttonWidth, buttonHeight);
-			panel.add(welcome);
+			icbmImage.add(welcome);
 			welcome.setFont(new Font("Arial", Font.PLAIN, 28));
 			welcome.setBounds(102, 50, 820, 32);
-			panel.add(author);
-			author.setBounds(462, 85, 100, 14);
+			welcome.setForeground(Color.WHITE);
+			icbmImage.add(author);
+			author.setBounds(452, 85, 120, 14);
+			author.setForeground(Color.WHITE);
+			icbmImage.add(author2);
+			author2.setBounds(472, 102, 80, 14);
+			author2.setForeground(Color.WHITE);
+			
+		}
+		
+		JLabel initializeImage() {
+			try {
+				return new JLabel(new ImageIcon(ImageIO.read(new File(getClass().getResource("/icbm.jpg").toURI()))));
+			} catch (IOException | URISyntaxException e) {
+				e.printStackTrace();
+			}
+			return null;
 		}
 
 		@Override
 		public void remove() {
-			panel.remove(startButton);
-			panel.remove(exitButton);
-			panel.remove(welcome);
-			panel.remove(author);
+			panel.remove(icbmImage);
 		}
 	}
 
